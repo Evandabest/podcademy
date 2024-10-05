90439146bc11ac361ea30ad774f0c44717e5e761
Emujin <emujintsogtjargal@gmail.com>
2024-10-05 18:51:22 -0400

    add: tts.py

---
+++ api/tts.py
@@ -0,0 +1,54 @@
+# The 'requests' and 'json' libraries are imported. 
+# 'requests' is used to send HTTP requests, while 'json' is used for parsing the JSON data that we receive from the API.
+import requests
+import json
+import os
+import uuid
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
+
+voices = {
+    "female1": "LcfcDJNUP1GQjkzn1xUU",
+    "female2": "oWAxZDx7w5VEj9dCyTzz",
+    "male1" : "nPczCjzI2devNBz1zQrb",
+    "male2" : "TX3LPaxmHKxFdv7VOQHJ",
+}
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+client = ElevenLabs(
+    api_key=ELEVENLABS_API_KEY,
+)
+
+
+def text_to_speech_file(text: str) -> str:
+    # Calling the text_to_speech conversion API with detailed parameters
+    response = client.text_to_speech.convert(
+        voice_id="pNInz6obpgDQGcFmaJgB", # Adam pre-made voice
+        output_format="mp3_22050_32",
+        text=text,
+        model_id="eleven_turbo_v2_5", # use the turbo model for low latency
+        voice_settings=VoiceSettings(
+            stability=0.0,
+            similarity_boost=1.0,
+            style=0.0,
+            use_speaker_boost=True,
+        ),
+    )
+
+    # uncomment the line below to play the audio back
+    # play(response)
+
+    # Generating a unique file name for the output MP3 file
+    save_file_path = f"{uuid.uuid4()}.mp3"
+
+    # Writing the audio to a file
+    with open(save_file_path, "wb") as f:
+        for chunk in response:
+            if chunk:
+                f.write(chunk)
+
+    print(f"{save_file_path}: A new audio file was saved successfully!")
+
+    # Return the path of the saved audio file
+    return save_file_path
+
