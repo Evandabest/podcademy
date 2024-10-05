006ecc2f99e24d0b894fcf70a373b05d9669acd1
Evandabest <evanhaque1@gmail.com>
2024-10-05 18:52:59 -0400

    updated login page for error handling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,9 +4,9 @@ export default function LoginPage() {
   return (
     <form>
       <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
+      <input className='text-black' id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <input className='text-black' id="password" name="password" type="password" required />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
